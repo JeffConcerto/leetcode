ae6662e7fa61ecd76095762388029776da16531a
JeffConcerto <111106355+JeffConcerto@users.noreply.github.com>
2022-08-22 13:10:41 -0400

    Solution

--- swift/1455 - Check If a Word Occurs As a Prefix of Any Word in a Sentence.swift
+++ swift/1455 - Check If a Word Occurs As a Prefix of Any Word in a Sentence.swift
@@ -1,9 +1,43 @@
+// Method 1 - New School
 class Solution {
     func isPrefixOfWord(_ sentence: String, _ searchWord: String) -> Int {
         let sentenceArray = sentence.components(separatedBy: " ")
         for (index, word) in sentenceArray.enumerated() {
             if word.hasPrefix(searchWord) { return index + 1 }
         }
+        return -1
+    }
+}
+
+// Method 2 - Old School
+
+class Solution {
+    func isPrefixOfWord(_ sentence: String, _ searchWord: String) -> Int {
+    
+        var containsPrefix = true
+        var wordIndexCount = 0
+        var wordArray = Array(searchWord)
+        var wordIndex = 0
+        
+        for char in sentence {
+            if containsPrefix == false && char != " " { continue }
+            
+            if char == " " { 
+                containsPrefix = true
+                wordIndex += 1
+                wordIndexCount = 0
+                continue 
+            }
+            
+            if char == wordArray[wordIndexCount] {
+                wordIndexCount += 1
+            } else {
+                containsPrefix = false
+            }
+            
+            if wordIndexCount == searchWord.count && containsPrefix { return wordIndex + 1 }
+        }
+        
         return -1
     }
 }
