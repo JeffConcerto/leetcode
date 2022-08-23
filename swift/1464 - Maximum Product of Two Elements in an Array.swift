62109e9279d9f7bcd7afa2af73cbfd1fd168e678
JeffConcerto <111106355+JeffConcerto@users.noreply.github.com>
2022-08-23 12:55:08 -0400

    Solution

---
+++ swift/1464 - Maximum Product of Two Elements in an Array.swift
@@ -0,0 +1,7 @@
+class Solution {
+    func maxProduct(_ nums: [Int]) -> Int {
+        var sortedNums = nums.sorted(by: >)
+        
+        return (sortedNums[0] - 1) * (sortedNums[1] - 1)
+    }
+}
