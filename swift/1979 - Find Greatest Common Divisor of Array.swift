89160ed2efb5b944cb11ea50c5181c5c516ab3ca
JeffConcerto <111106355+JeffConcerto@users.noreply.github.com>
2022-09-21 12:53:58 -0400

    Solution

---
+++ swift/1979 - Find Greatest Common Divisor of Array.swift
@@ -0,0 +1,20 @@
+class Solution {
+    func findGCD(_ nums: [Int]) -> Int {
+        let nums = nums.sorted()
+        let minNum = nums[0]
+        let maxNum = nums.last!
+        
+        return getGCD(minNum: minNum, maxNum: maxNum)
+    }
+    
+    func getGCD(minNum: Int, maxNum: Int) -> Int {
+        var divisor = maxNum
+        
+        while divisor > 0 {
+            if minNum % divisor  == 0 && maxNum % divisor == 0 { return divisor }
+            divisor -= 1
+        }
+        
+        return 1
+    }
+}
