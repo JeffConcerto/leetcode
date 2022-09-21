b174f09a8d817d8ef1bad34f413a48c1b8780f42
JeffConcerto <111106355+JeffConcerto@users.noreply.github.com>
2022-09-21 12:39:32 -0400

    Solution

--- swift/1991 - Find the Middle Index in Array.swift
+++ swift/1991 - Find the Middle Index in Array.swift
@@ -2,16 +2,15 @@ class Solution {
     func findMiddleIndex(_ nums: [Int]) -> Int {
         var firstHalf = nums.reduce(0,+)
         var secondHalf = 0
-        var index = nums.count - 1
-        var leftMostIndex = -1
+        var index = 0
         
-        for num in nums.reversed() {
-            if firstHalf - num == secondHalf { leftMostIndex = index }
+        for num in nums {
+            if firstHalf - num == secondHalf { return index }
             secondHalf += num
             firstHalf -= num
-            index -= 1
+            index += 1
         }
         
-        return leftMostIndex
+        return -1
     }
 }
