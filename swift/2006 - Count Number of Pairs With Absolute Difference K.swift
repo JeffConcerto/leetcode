5e816d1ee8fd5770aaa46bf066713d60e8033741
JeffConcerto <111106355+JeffConcerto@users.noreply.github.com>
2022-09-22 12:17:50 -0400

    Solution

---
+++ swift/2006 - Count Number of Pairs With Absolute Difference K.swift
@@ -0,0 +1,13 @@
+class Solution {
+    func countKDifference(_ nums: [Int], _ k: Int) -> Int {
+        var count = 0
+        for i in 0..<nums.count {
+            for j in i+1..<nums.count {
+                if abs(nums[i]-nums[j]) == k { count += 1 }
+            }
+        }
+        
+        return count
+        
+    }
+}
