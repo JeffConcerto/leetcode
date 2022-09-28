b0493f436b5d1fe50ed53b466185320f0419bea8
JeffConcerto <111106355+JeffConcerto@users.noreply.github.com>
2022-09-28 12:40:28 -0400

    Solution

--- swift/2062 - Count Vowel Substrings of a String.swift
+++ swift/2062 - Count Vowel Substrings of a String.swift
@@ -1,3 +1,4 @@
+// Method 1
 class Solution {
     func countVowelSubstrings(_ word: String) -> Int {
         var word = Array(word)
@@ -30,6 +31,42 @@ class Solution {
                 // do nothing
             } else { break }
                 
+                j += 1
+            }
+        }
+        
+        return count
+    }
+}
+
+
+// Method 2
+class Solution {
+    func countVowelSubstrings(_ word: String) -> Int {
+        var word = Array(word)
+        var set: Set<Character> = Set(["a", "e", "i", "o", "u"])
+        var count = 0
+        
+        for i in 0..<word.count {
+            if i + 4 >= word.count { break }
+            var substring: Set<Character> = []
+            var j = i + 4
+            var isValid = true
+            
+            for index in i...j {
+                substring.insert(word[index])
+                if !set.contains(word[index]) { isValid = false}
+            }
+            if !isValid { continue }
+            
+            if substring == set { count += 1 }
+            
+            j += 1
+            
+            while j < word.count {
+                if !set.contains(word[j]) { break }
+                substring.insert(word[j])
+                if substring == set { count += 1}
                 j += 1
             }
         }
