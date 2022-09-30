a358e67fc7a7d9effa6a722f941f885932b8b29b
JeffConcerto <111106355+JeffConcerto@users.noreply.github.com>
2022-09-30 12:34:42 -0400

    Solution

--- swift/2094 - Finding 3-Digit Even Numbers.swift
+++ swift/2094 - Finding 3-Digit Even Numbers.swift
@@ -1,17 +1,17 @@
 class Solution {
     func findEvenNumbers(_ digits: [Int]) -> [Int] {
-        var result = [Int]()
+        var result: Set<Int> = []
         for i in 0..<digits.count {
-            if digits[i] == 0 { break }
+            if digits[i] == 0 { continue }
             for j in 0..<digits.count where j != i {
                 
                 for k in 0..<digits.count where k != j && k != i && digits[k] % 2 == 0 {
                     let numAsString = "\(digits[i])\(digits[j])\(digits[k])"
-                    result.append(Int(numAsString)!)
+                    result.insert(Int(numAsString)!)
                 }
             }
         }
         
-        return result.sorted()
+        return Array(result).sorted()
     }
 }
