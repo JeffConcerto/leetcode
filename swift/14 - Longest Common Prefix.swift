25ed36eefba551b5e11be76c0c5b221e37da469b
JeffConcerto <111106355+JeffConcerto@users.noreply.github.com>
2022-11-08 12:20:39 -0500

    Solution

--- swift/14 - Longest Common Prefix.swift
+++ swift/14 - Longest Common Prefix.swift
@@ -16,3 +16,27 @@ class Solution {
         
     }
 }
+
+// Method 2
+class Solution {
+    func longestCommonPrefix(_ strs: [String]) -> String {
+        
+        var index = 0
+        var isCommon = true
+        
+        while isCommon && index <= strs[0].count  {
+            var currentPrefix = strs[0].prefix(index + 1)
+            
+            for i in 1..<strs.count {
+                if strs[i].prefix(index + 1) != currentPrefix {
+                    isCommon = false
+                    break
+                }
+            }
+            
+            if isCommon { index += 1 }
+        }
+        
+        return String(strs[0].prefix(index))
+    }
+}
