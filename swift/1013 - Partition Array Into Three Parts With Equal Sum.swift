bf49f5b476eed43d0ffd5a82125496098221f39f
JeffConcerto <111106355+JeffConcerto@users.noreply.github.com>
2022-11-09 12:25:31 -0500

    Solution

--- swift/1013 - Partition Array Into Three Parts With Equal Sum.swift
+++ swift/1013 - Partition Array Into Three Parts With Equal Sum.swift
@@ -1,1 +1,21 @@
-//Wrong
+class Solution {
+    func canThreePartsEqualSum(_ arr: [Int]) -> Bool {
+        var totalSum = arr.reduce(0, +)
+        guard totalSum % 3 == 0 else { return false }
+        let goalSum = totalSum / 3        
+        var currentSum = 0
+        var matchCount = 0
+        
+        for num in arr {
+            if currentSum == Int.min { currentSum = 0 }
+            currentSum += num
+            if currentSum == goalSum && matchCount < 2 {
+                currentSum = Int.min
+                matchCount += 1
+            }
+        }        
+        if currentSum == goalSum { matchCount += 1 }
+        
+        return matchCount == 3
+    }
+}
