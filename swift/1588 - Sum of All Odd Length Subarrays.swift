0b2b342dbfe4f9083f3cf62dec55dc34182c33dc
JeffConcerto <111106355+JeffConcerto@users.noreply.github.com>
2022-11-16 11:08:06 -0500

    Solution

--- swift/1588 - Sum of All Odd Length Subarrays.swift
+++ swift/1588 - Sum of All Odd Length Subarrays.swift
@@ -1,3 +1,4 @@
+// Method 1:
 class Solution {
     func sumOddLengthSubarrays(_ arr: [Int]) -> Int {
         var totalSum = 0
@@ -9,6 +10,28 @@ class Solution {
                 tempSum += arr[j]
                 if j + 1 >= i { totalSum += tempSum}
             }
+        }
+        
+        return totalSum
+    }
+}
+
+// Method 2:
+class Solution {
+    func sumOddLengthSubarrays(_ arr: [Int]) -> Int {
+        var totalSum = 0
+        
+        for i in 1...arr.count {
+            if i % 2 == 0 { continue }
+            var tempSum = 0
+            for j in 0..<arr.count {
+                if j >= i { 
+                    totalSum += tempSum
+                    tempSum -= arr[j - i]
+                }
+                tempSum += arr[j]
+            }
+            totalSum += tempSum
         }
         
         return totalSum
