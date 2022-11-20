5829ddc3742b6e484fec53530d2cac7f1b43cbb5
JeffConcerto <111106355+JeffConcerto@users.noreply.github.com>
2022-11-20 11:53:03 -0500

    Solution

--- swift/18 - 4Sum.swift
+++ swift/18 - 4Sum.swift
@@ -60,3 +60,70 @@ class Solution {
     }
 }
 
+
+// From Memory/Understanding - 24 Hours Later:
+class Solution {
+    func fourSum(_ nums: [Int], _ target: Int) -> [[Int]] {
+        var nums = nums.sorted()
+        
+        return kSum(nums, target, 4)
+    }
+    
+    private func kSum(_ nums: [Int], _ target: Int, _ k: Int) -> [[Int]] {
+        guard !nums.isEmpty else { return [] }
+        
+        let avgValue = target / k
+        
+        guard nums[0] <= avgValue && nums.last! >= avgValue else { return [] }
+        
+        guard k > 2 else { return twoSum(nums, target) }
+        
+        var result = [[Int]]()
+        
+        for i in 0..<nums.count {
+            guard i == 0 || nums[i] != nums[i-1] else {
+                continue
+            }
+            
+            for subset in kSum(Array(nums[i+1..<nums.count]), target - nums[i], k - 1) {
+                result.append([nums[i]] + subset)
+            }
+        }
+        
+        
+        return result
+    }
+    
+    private func twoSum(_ nums: [Int], _ target: Int) -> [[Int]] {
+        var result = [[Int]]()
+        var low = 0
+        var high = nums.count - 1
+        
+        while low < high {
+            guard low == 0 || nums[low] != nums[low-1] else {
+                low += 1
+                continue
+            }
+            guard high == nums.count - 1 || nums[high] != nums[high+1] else {
+                high -= 1
+                continue
+            }
+            
+            let sum = nums[low] + nums[high]
+            
+            if sum < target {
+                low += 1
+            } else if sum > target {
+                high -= 1
+            } else {
+                result.append([nums[low], nums[high]])
+                low += 1
+                high -= 1
+            }
+        }
+        
+        return result
+    }  
+}
+
+
