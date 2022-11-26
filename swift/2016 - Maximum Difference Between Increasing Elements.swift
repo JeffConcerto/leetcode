7fcf5493c5ebc95a5f8764ca03f3210e1fa85dbf
JeffConcerto <111106355+JeffConcerto@users.noreply.github.com>
2022-11-26 11:12:34 -0500

    Solution

--- swift/2016 - Maximum Difference Between Increasing Elements.swift
+++ swift/2016 - Maximum Difference Between Increasing Elements.swift
@@ -1,3 +1,4 @@
+// Method 1 - O(N^2):
 class Solution {
     func maximumDifference(_ nums: [Int]) -> Int {
         var maxDiff = -1
@@ -8,6 +9,23 @@ class Solution {
             }
         }
 
+        
+        return maxDiff
+    }
+}
+
+// Method 2 - O(N):
+class Solution {
+    func maximumDifference(_ nums: [Int]) -> Int {
+        var maxNum = nums.last!
+        var maxDiff = -1
+        
+        for i in stride(from: nums.count-2, to: -1, by: -1) {
+            if maxNum == nums[i] { continue }
+            maxDiff = max(maxDiff, maxNum - nums[i])
+            maxNum = max(maxNum, nums[i])
+        }
+        
         
         return maxDiff
     }
