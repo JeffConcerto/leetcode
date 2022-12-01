4de104a12e805fd26d0d328b6a6cf219fdef2bea
JeffConcerto <111106355+JeffConcerto@users.noreply.github.com>
2022-12-01 11:19:49 -0500

    Solution

--- swift/2455 - Average Value of Even Numbers That Are Divisible by Three.swift
+++ swift/2455 - Average Value of Even Numbers That Are Divisible by Three.swift
@@ -27,3 +27,19 @@ class Solution {
         return sum > 0 ? sum / nums.count : 0
     }
 }
+
+// Method 3:
+
+class Solution {
+    func averageValue(_ nums: [Int]) -> Int {
+       var sumCount = (sum: 0, count: 0)
+        
+        for i in 0..<nums.count {
+            guard nums[i] % 3 == 0 && nums[i] % 2 == 0 else { continue }
+            sumCount.sum += nums[i]
+            sumCount.count += 1
+        }
+        
+        return sumCount.count > 0 ? sumCount.sum / sumCount.count : 0
+    }
+}
