092665b174c5fd3be233fc21413469cb6f44992e
JeffConcerto <111106355+JeffConcerto@users.noreply.github.com>
2022-12-02 12:25:09 -0500

    Solution

--- swift/39 - Combination Sum.swift
+++ swift/39 - Combination Sum.swift
@@ -7,15 +7,13 @@ class Solution {
     }
     
     private func helper(_ candidates: [Int], _ target: Int, _ selected: [Int], _ result: inout [[Int]]) {
-        guard target > 0 else {
-            if target < 0 { return }
+        guard !candidates.isEmpty && target >= 0 else { return }
+        
+         if target == 0 {
             result.append(selected)
-            return
+        } else {
+            helper(candidates, target - candidates[0], selected + [candidates[0]], &result)
+            helper(Array(candidates[1..<candidates.count]), target, selected, &result)
         }
-        
-        guard !candidates.isEmpty else { return }
-        
-        helper(candidates, target - candidates[0], selected + [candidates[0]], &result)
-        helper(Array(candidates[1..<candidates.count]), target, selected, &result)
     }
 }
