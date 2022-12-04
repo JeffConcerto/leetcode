0f992baa992984acbf4861fdc1f656179cae5c3f
JeffConcerto <111106355+JeffConcerto@users.noreply.github.com>
2022-12-04 13:24:02 -0500

    Solution

---
+++ swift/0040 - Combination Sum II.swift
@@ -0,0 +1,35 @@
+class Solution {
+    func combinationSum2(_ candidates: [Int], _ target: Int) -> [[Int]] {
+        var result: Set<[Int]> = []
+        var candidates = candidates.sorted()
+        let total = candidates.reduce(0,+)
+        if total < target { return []}
+        if total == target { return [candidates]}
+      
+        helper(candidates, target, [], &result, total)
+        
+        return Array(result)
+    }
+    
+    private func helper(_ candidates: [Int], _ target: Int, _ selection: [Int], _ result: inout Set<[Int]>, _ total: Int) {
+        guard target != 0 else { result.insert(selection); return }
+        guard total >= target else { return }
+        guard total != target else {
+            let selection = selection + candidates
+            result.insert(selection)
+            return
+        }
+        guard !candidates.isEmpty && target > 0 else { return }
+                
+        var nextIndex = 1
+        var total = total
+      
+        helper(Array(candidates[nextIndex..<candidates.count]), target - candidates[0], selection + [candidates[0]], &result, total - candidates[0])
+          while nextIndex < candidates.count && candidates[0] == candidates[nextIndex] {
+            total -= candidates[nextIndex]
+            nextIndex += 1
+        }
+      
+        helper(Array(candidates[nextIndex..<candidates.count]), target, selection, &result, total - candidates[0])
+    }
+}
