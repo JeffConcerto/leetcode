7e9dde9e7df018ec306edd500f45b65e99f78bd6
JeffConcerto <111106355+JeffConcerto@users.noreply.github.com>
2022-12-20 12:07:42 -0500

    Solution

--- swift/1790 - Check if One String Swap Can Make Strings Equal.swift
+++ swift/1790 - Check if One String Swap Can Make Strings Equal.swift
@@ -1,3 +1,4 @@
+// Method 1:
 class Solution {
     func areAlmostEqual(_ s1: String, _ s2: String) -> Bool {
         guard s1 != s2 else { return true }
@@ -17,5 +18,29 @@ class Solution {
         }
         
         return s1Map == s2Map
+    }
+}
+
+// Method 2:
+class Solution {
+    func areAlmostEqual(_ s1: String, _ s2: String) -> Bool {
+        guard s1 != s2 else { return true }
+        guard s1.count == s2.count else { return false }
+        
+        var s1 = Array(s1)
+        var s2 = Array(s2)
+        var indices = [Int]()
+
+        for i in 0..<s1.count {
+            if s1[i] != s2[i] {
+                indices.append(i)
+            }
+        }
+
+        guard indices.count == 2 else { return false }
+
+        s1.swapAt(indices[0], indices[1])
+
+        return s1 == s2
     }
 }
