fb004e0e39ea03816deef3962cf3642df67ebe23
JeffConcerto <111106355+JeffConcerto@users.noreply.github.com>
2022-12-21 12:29:50 -0500

    Solution

--- swift/1945 - Sum of Digits of String After Convert.swift
+++ swift/1945 - Sum of Digits of String After Convert.swift
@@ -24,3 +24,40 @@ class Solution {
         return sum
     }
 }
+
+// Method 2:
+class Solution {
+    func getLucky(_ s: String, _ k: Int) -> Int {
+        var numberAsString = convertToNumberString(s)
+        
+        // Add up the digits k Times:
+        var integer = 0
+        for _ in 0..<k {
+            integer = sumAllDigits(of: numberAsString)
+            numberAsString = String(integer)
+        }
+        return integer
+    }
+
+    private func convertToNumberString(_ string: String) -> String {
+        let letterAValue = Character("a").asciiValue!
+        var result = ""
+        
+        for char in string {
+            let convertedNumberAsString = String(Int(char.asciiValue! - letterAValue + 1))
+            result.append(convertedNumberAsString)
+        }
+
+        return result
+    } 
+
+    private func sumAllDigits(of string: String) -> Int {
+        var sum = 0
+
+       for char in string {
+           sum += char.wholeNumberValue!
+       }
+
+        return sum
+    }
+}
