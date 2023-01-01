edd26fde07d162a9a2fc1b1cec5131c062b92f63
JeffConcerto <111106355+JeffConcerto@users.noreply.github.com>
2023-01-01 11:59:03 -0500

    Solution

--- swift/1512 - Number of Good Pairs.swift
+++ swift/1512 - Number of Good Pairs.swift
@@ -11,3 +11,26 @@ class Solution {
         return goodPairCount
     }
 }
+
+// METHOD 2:
+class Solution {
+    func numIdenticalPairs(_ nums: [Int]) -> Int {
+        var hash = [Int: Int]()
+
+        for i in 0..<nums.count {
+            hash[nums[i], default: -1] += 1
+        }
+
+        return hash.values.map {factorial($0)}.reduce(0, +)
+    }
+
+    private func factorial(_ n: Int) -> Int {
+        var n = n
+        var sum = 0
+        while n > 0 {
+            sum += n
+            n -= 1
+        }
+        return sum
+    }
+}
