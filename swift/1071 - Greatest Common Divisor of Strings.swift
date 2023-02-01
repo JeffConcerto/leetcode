7d0fb47426ab5bbd15db3fa0d957cc4c271de923
JeffConcerto <111106355+JeffConcerto@users.noreply.github.com>
2023-02-01 12:44:54 -0500

    Solution

--- swift/1071 - Greatest Common Divisor of Strings.swift
+++ swift/1071 - Greatest Common Divisor of Strings.swift
@@ -1,3 +1,35 @@
+// Solution 1:
+class Solution {
+    func gcdOfStrings(_ str1: String, _ str2: String) -> String {
+        let smallString = str1.count >= str2.count ? str2 : str1
+        let largeString = str1.count >= str2.count ? str1 : str2
+        var smallCount = smallString.count
+        let largeCount = largeString.count
+
+        let gcd = gcd(largeCount, smallCount)
+
+        var largeTest = ""
+        for _ in 0..<largeCount / gcd {
+            largeTest += String(smallString.prefix(gcd))
+        }
+
+        guard largeTest == largeString else { return "" }
+
+        var smallTest = ""
+        for _ in 0..<smallString.count / gcd {
+            smallTest += String(smallString.prefix(gcd))
+        }
+
+        return smallTest == smallString ? String(smallString.prefix(gcd)) : ""
+    }
+
+    private func gcd(_ a: Int, _ b: Int) -> Int {
+        if a % b == 0 { return b }
+        return gcd(b, a % b)
+    }
+}
+
+// Solution 2:
 class Solution {
     func gcdOfStrings(_ str1: String, _ str2: String) -> String {
         let str1 = Array(str1)
