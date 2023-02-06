e6e02a87b83151352b6b88b50507338bd8b95db1
JeffConcerto <111106355+JeffConcerto@users.noreply.github.com>
2023-02-06 11:58:06 -0500

    Solution

--- swift/1470 - Shuffle the Array.swift
+++ swift/1470 - Shuffle the Array.swift
@@ -1,3 +1,4 @@
+// Solution 1:
 class Solution {
     func shuffle(_ nums: [Int], _ n: Int) -> [Int] {
         var result = [Int]()
@@ -10,6 +11,28 @@ class Solution {
             index += 1
         }
         
+        return result
+    }
+}
+
+// Soultuon 2:
+class Solution {
+    func shuffle(_ nums: [Int], _ n: Int) -> [Int] {
+        // Split array into two halves:
+        let nums1 = Array(nums[0..<n])
+        let nums2 = Array(nums[n...nums.count-1])
+        
+        var result = [Int]()
+
+        // Merge arrays into single array:
+        for i in 0..<nums.count {
+            if i % 2 == 0 {
+              result.append(nums1[i/2])  
+            } else {
+                result.append(nums2[i/2])
+            }
+        }
+
         return result
     }
 }
