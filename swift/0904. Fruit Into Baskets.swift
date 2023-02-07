556399887702c7b189288c31508acced005c5c57
JeffConcerto <111106355+JeffConcerto@users.noreply.github.com>
2023-02-07 13:05:25 -0500

    Solution

--- swift/0904. Fruit Into Baskets.swift
+++ swift/0904. Fruit Into Baskets.swift
@@ -1,3 +1,4 @@
+// Solution #1:
 class Solution {
     func totalFruit(_ fruits: [Int]) -> Int {
         var fruitSet = Set<Int>()
@@ -14,5 +15,58 @@ class Solution {
         }
 
         return maxFruit
+    }
+}
+
+// Solution #2:
+class Solution {
+    func totalFruit(_ fruits: [Int]) -> Int {
+       var basketMap = [Int: Int]()
+       var left = 0
+
+       for (right, fruit) in fruits.enumerated() {
+           basketMap[fruit, default: 0] += 1
+
+           if basketMap.count > 2 {
+               basketMap[fruits[left]]! -= 1
+
+               if basketMap[fruits[left]]! == 0 {
+                   basketMap[fruits[left]] = nil
+               }
+
+               left += 1
+           }
+       }
+
+       return fruits.count - left 
+    }
+}
+
+// Solution #3:
+class Solution {
+    func totalFruit(_ fruits: [Int]) -> Int {
+       var basketMap = [Int: Int]()
+       var left = 0
+       var maxFruit = 0
+
+       for (right, fruit) in fruits.enumerated() {
+           basketMap[fruit, default: 0] += 1
+
+           while basketMap.count > 2 {
+               let fruit = fruits[left]
+               if let count = basketMap[fruit] {
+                   if count == 1 {
+                       basketMap[fruit] = nil
+                   } else {
+                       basketMap[fruit] = count-1
+                   }
+               }
+               left += 1
+           }
+
+           maxFruit = max(maxFruit, right - left + 1)
+       }
+
+       return maxFruit
     }
 }
