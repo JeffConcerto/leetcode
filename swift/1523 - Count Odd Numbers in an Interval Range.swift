6e7489775abebf215c9ad82ecf92a9ef3a4276cb
JeffConcerto <111106355+JeffConcerto@users.noreply.github.com>
2023-02-13 11:55:30 -0500

    Solution

--- swift/1523 - Count Odd Numbers in an Interval Range.swift
+++ swift/1523 - Count Odd Numbers in an Interval Range.swift
@@ -1,3 +1,18 @@
+// Programming Solution:
+class Solution {
+    func countOdds(_ low: Int, _ high: Int) -> Int {
+        var count = 0
+        
+        for num in low...high where num % 2 == 1 {
+            count += 1
+        }
+
+        return count
+    }
+}
+
+
+// Math Solution:
 class Solution {
     func countOdds(_ low: Int, _ high: Int) -> Int {
         var count = (high - low) / 2
