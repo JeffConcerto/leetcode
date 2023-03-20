045840d703adf58c8d8364bdb6e1686bd91c9b15
JeffConcerto <111106355+JeffConcerto@users.noreply.github.com>
2023-03-20 12:13:03 -0400

    Solution

--- swift/0211 - Design Add and Search Words Data Structure.swift
+++ swift/0211 - Design Add and Search Words Data Structure.swift
@@ -1,76 +1,48 @@
+
+class WordDictionary {
+    class TrieNode {
+        var isWord = false
+        var children = [Character: TrieNode]()
+    }
+
+    private var root = TrieNode()
+
+    init() {}
+    
+    func addWord(_ word: String) {
+        var node = root
+        for char in word {
+            if node.children[char] == nil {
+                node.children[char] = TrieNode()
+            }
+            node = node.children[char]!
+        }
+        node.isWord = true
+    }
+    
+    func search(_ word: String) -> Bool {
+        return findWord(0, Array(word), root)
+    }
+
+    private func findWord(_ index: Int, _ word: [Character], _ node: TrieNode) -> Bool {
+        guard index < word.count else { return node.isWord }
+
+        if word[index] == "." { 
+            for childNode in node.children.values {
+               let found = findWord(index+1, word, childNode)
+               if found { return true}
+            }
+        } else if let nextNode = node.children[word[index]] {
+            return findWord(index+1, word, nextNode)
+        }
+
+        return false
+    }
+}
+
 /**
  * Your WordDictionary object will be instantiated and called as such:
  * let obj = WordDictionary()
  * obj.addWord(word)
  * let ret_2: Bool = obj.search(word)
  */
-
-class WordDictionary {
-
-    var trie = Trie()
-    init() {}
-    
-    func addWord(_ word: String) {
-        for char in word {
-            if char == "." { return }
-        }
-        trie.insert(word)
-    }
-    
-    func search(_ word: String) -> Bool {
-        trie.dfs(word)
-    }
-
-
-    class Trie {
-        class Node {
-            var isWord: Bool = false
-            var children: [Character: Node] = [:]
-        }
-
-         private var root = Node()
-
-        init() {}
-
-        func insert(_ word: String) {
-            var node = root
-            for char in word {
-                node.children[char] = node.children[char] ?? Node()
-                node = node.children[char]!
-            }
-            node.isWord = true
-        }
-        
-        func dfs(_ word: String) -> Bool {
-            return dfs_(0, Array(word), root)
-        }
-        private func dfs_(_ index: Int, _ word: [Character], _ node: Trie.Node) -> Bool {
-            if index == word.count { return node.isWord }
-            let char = word[index]
-
-            if char == "." {
-                for character in node.children.keys {
-                    if let nextNode = node.children[character], dfs_(index+1, word, nextNode) {
-                        return true 
-                    }
-                }
-                return false
-            } else {
-                if let nextNode = node.children[char], dfs_(index+1,word,nextNode) {
-                    return true
-                } else {
-                    return false
-                }
-            }
-        }
-    }
-
-
-}
-
-// /**
-//  * Your WordDictionary object will be instantiated and called as such:
-//  * let obj = WordDictionary()
-//  * obj.addWord(word)
-//  * let ret_2: Bool = obj.search(word)
-//  */
