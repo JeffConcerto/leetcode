81bd145a7c8041e68dc17eeb76163d98c5ee6d0c
JeffConcerto <111106355+JeffConcerto@users.noreply.github.com>
2023-05-01 11:53:49 -0400

    Update 1491 - Average Salary Excluding the Minimum and Maximum Salary.swift

--- swift/1491 - Average Salary Excluding the Minimum and Maximum Salary.swift
+++ swift/1491 - Average Salary Excluding the Minimum and Maximum Salary.swift
@@ -1,3 +1,4 @@
+// Sort Solution:
 class Solution {
     func average(_ salary: [Int]) -> Double {
         let salarySorted = Array(salary.sorted()[1..<salary.count - 1])
@@ -5,5 +6,31 @@ class Solution {
         
         return Double(total) / Double(salarySorted.count)
         
+    }
+}
+
+// Swift Solution:
+class Solution {
+    func average(_ salary: [Int]) -> Double {
+        let maxSalary = salary.max() ?? 0
+        let minSalary = salary.min() ?? 0
+        let total = salary.reduce(0,+)
+        return Double((total-minSalary-maxSalary)) / Double(salary.count-2)
+    }
+}
+
+// Manual Solution:
+class Solution {
+    func average(_ salary: [Int]) -> Double {
+        var maxSalary = 0
+        var minSalary = Int.max
+        var total = 0
+        for money in salary {
+            total += money
+            maxSalary = max(maxSalary, money)
+            minSalary = min(minSalary,money)
+        }
+
+        return Double((total-minSalary-maxSalary)) / Double(salary.count-2)
     }
 }
