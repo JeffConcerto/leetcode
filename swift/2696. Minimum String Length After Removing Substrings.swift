1184d7f237fc05870f8c4f861956f1bd499fb709
JeffConcerto <111106355+JeffConcerto@users.noreply.github.com>
2023-05-22 12:47:34 -0400

    Update 2696. Minimum String Length After Removing Substrings.swift

--- swift/2696. Minimum String Length After Removing Substrings.swift
+++ swift/2696. Minimum String Length After Removing Substrings.swift
@@ -4,23 +4,18 @@ class Solution {
         var left = 0
         
         while left < s.count-1 {
-                if s[left] == "A" {
-                    if s[left+1] == "B" {
-                        s.remove(at:left)
-                        s.remove(at:left)
-                        left = 0 
-                        continue
-                    }
-                } else if s[left] == "C" {
-                    if s[left+1] == "D" {
-                        s.remove(at:left)
-                        s.remove(at:left)
-                        left = 0 
-                        continue
-                    }
-                }
+            if s[left] == "A" && s[left+1] == "B" {
+                s.remove(at:left)
+                s.remove(at:left)
+                left = 0 
+            } else if s[left] == "C" && s[left+1] == "D" {
+                s.remove(at:left)
+                s.remove(at:left)
+                left = 0 
+            } else {
                 left += 1
             }
+        }
 
         return s.count
     }
