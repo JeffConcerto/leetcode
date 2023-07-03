f1b85fe4d268bacb19dc57dda3d885fcaee8b398
JeffConcerto <111106355+JeffConcerto@users.noreply.github.com>
2023-07-03 02:00:49 -0400

    Update 0859 - Buddy Strings.swift

--- swift/0859 - Buddy Strings.swift
+++ swift/0859 - Buddy Strings.swift
@@ -1,3 +1,46 @@
+// New Answer:
+class Solution {
+    func buddyStrings(_ s: String, _ goal: String) -> Bool {
+        
+        if s == goal {
+            return sameStringsCan(s, goal)
+        } else {
+            return differentStringsCan(s,goal)
+        }
+    }
+
+    private func differentStringsCan(_ s: String, _ goal: String) -> Bool {
+        var s = Array(s)
+        let goal = Array(goal)
+
+        var left = -1
+        var right = -1
+        for i in 0..<s.count {
+            if s[i] != goal[i] {
+                if left == -1 {
+                    left = i
+                } else {
+                    right = i
+                }
+            }
+            if left != -1 && right != -1 { break }
+        }
+        if right == -1 { return false }
+        s.swapAt(left,right)
+        return s == goal 
+    }
+
+    private func sameStringsCan(_ s: String, _ goal: String) -> Bool {
+        var set = Set<Character>()
+        for letter in s {
+            if set.contains(letter) { return true}
+            set.insert(letter)
+        } 
+
+        return false
+    }
+}
+// Old Answer:
 class Solution {
     func buddyStrings(_ s: String, _ goal: String) -> Bool {
         guard s.count == goal.count && s.count != 1 else { return false }
