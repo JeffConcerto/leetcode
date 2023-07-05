fe2d9d15283f5af61cc7e264b3c707268ada8700
JeffConcerto <111106355+JeffConcerto@users.noreply.github.com>
2023-07-05 12:38:01 -0400

    Update 18 - 4Sum.swift

--- swift/18 - 4Sum.swift
+++ swift/18 - 4Sum.swift
@@ -1,129 +1,35 @@
-// NEEDED HELP TO GET SOLUTION:
 class Solution {
     func fourSum(_ nums: [Int], _ target: Int) -> [[Int]] {
         var nums = nums.sorted()
-        
-        return kSum(nums: nums, target: target, k: 4)
-    }
-    
-    private func kSum(nums: [Int], target: Int, k: Int) -> [[Int]] {
         var result = [[Int]]()
-        
-        // If we run out of numbers to add, return result:
-        guard !nums.isEmpty else { return result }
-        
-        // There are k remaining values to add to the sum.
-        // The average of these values is at least target / k:
-        let averageValue = target == 0 ? 0 : target / k
-        
-        // We cannnot obtain a sum of target if the smallest value
-        // in nums is greater than the averageValue nor if the
-        // largest value in nums is smaller than the averageValue:
-        if nums[0] > averageValue || nums.last! < averageValue {
-            return result
-        }
-        
-        if k == 2 { return twoSum(nums, target) }
-        
-        for i in 0..<nums.count {
+
+        for (i, num) in nums.enumerated() {
             if i > 0 && nums[i] == nums[i-1] { continue }
-            let updatedNums = Array(nums[i+1..<nums.count])
-            for subset in kSum(nums: updatedNums, target: target - nums[i], k: k-1) {
-                result.append([nums[i]] + subset)
+            var sum = 0
+            var count = result.count
+            for j in i+1..<nums.count {
+                if count < result.count && nums[j] == nums[j-1] { continue }
+                sum = nums[j] + nums[i]
+                var left = j+1
+                var right = nums.count-1
+                while left < right {
+                    let total = sum + nums[left] + nums[right]
+                    if total == target {
+                        result.append([num,nums[j],nums[left],nums[right]])
+                        left += 1
+                        while left < right && nums[left] == nums[left-1] {
+                            left += 1
+                        }
+                    } else if total < target {
+                        left += 1
+                    } else {
+                        right -= 1
+                    }
+                }
             }
         }
-        
-        return result
-    }
-    
-    private func twoSum(_ nums: [Int], _ target: Int) -> [[Int]] {
-        var result = [[Int]]()
-        var low = 0
-        var high = nums.count - 1
-        
-        while low < high {
-            let currentSum = nums[low] + nums[high]
-            
-            if currentSum < target || (low > 0 && nums[low] == nums[low-1]) {
-                low += 1
-            } else if currentSum > target || (high < nums.count - 1 && nums[high] == nums[high+1]) {
-                high -= 1
-            } else {
-                result.append([nums[low], nums[high]])
-                low += 1
-                high -= 1
-            }
-        }
-        
+
         return result
         
     }
 }
-
-
-// From Memory/Understanding - 24 Hours Later:
-class Solution {
-    func fourSum(_ nums: [Int], _ target: Int) -> [[Int]] {
-        var nums = nums.sorted()
-        
-        return kSum(nums, target, 4)
-    }
-    
-    private func kSum(_ nums: [Int], _ target: Int, _ k: Int) -> [[Int]] {
-        guard !nums.isEmpty else { return [] }
-        
-        let avgValue = target / k
-        
-        guard nums[0] <= avgValue && nums.last! >= avgValue else { return [] }
-        
-        guard k > 2 else { return twoSum(nums, target) }
-        
-        var result = [[Int]]()
-        
-        for i in 0..<nums.count {
-            guard i == 0 || nums[i] != nums[i-1] else {
-                continue
-            }
-            
-            for subset in kSum(Array(nums[i+1..<nums.count]), target - nums[i], k - 1) {
-                result.append([nums[i]] + subset)
-            }
-        }
-        
-        
-        return result
-    }
-    
-    private func twoSum(_ nums: [Int], _ target: Int) -> [[Int]] {
-        var result = [[Int]]()
-        var low = 0
-        var high = nums.count - 1
-        
-        while low < high {
-            guard low == 0 || nums[low] != nums[low-1] else {
-                low += 1
-                continue
-            }
-            guard high == nums.count - 1 || nums[high] != nums[high+1] else {
-                high -= 1
-                continue
-            }
-            
-            let sum = nums[low] + nums[high]
-            
-            if sum < target {
-                low += 1
-            } else if sum > target {
-                high -= 1
-            } else {
-                result.append([nums[low], nums[high]])
-                low += 1
-                high -= 1
-            }
-        }
-        
-        return result
-    }  
-}
-
-
