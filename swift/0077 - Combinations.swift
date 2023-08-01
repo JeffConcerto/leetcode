37b76bc6b34c00520c698b853db6dc4fe59dba1e
JeffConcerto <111106355+JeffConcerto@users.noreply.github.com>
2023-08-01 13:18:39 -0400

    Update 0077 - Combinations.swift

--- swift/0077 - Combinations.swift
+++ swift/0077 - Combinations.swift
@@ -1,26 +1,21 @@
 class Solution {
     func combine(_ n: Int, _ k: Int) -> [[Int]] {
-        let nums = Array(1...n)
         var result = [[Int]]()
-        
-        func _combine(_ numsLeft: [Int], _ chosen: [Int]) {
-            guard chosen.count != k else { result.append(chosen); return } 
-            guard numsLeft.count + chosen.count >= k else { return }
 
-            let nextNumsLeft =  Array(numsLeft[1..<numsLeft.count])
-            _combine(nextNumsLeft, chosen)
-            
-            let nextNum = numsLeft[0]
-            var chosen = chosen
-            chosen.append(nextNum)
-            _combine(nextNumsLeft, chosen)
+        func dfs(_ i: Int, _ current: [Int]) {
+            if current.count == k {
+                result.append(current)
+            } else if i > n {
+                return
+            } else {
+                dfs(i+1, current + [i])
+                dfs(i+1, current)
+            }
         }
 
-        for i in 0..<nums.count {
-            let numsLeft = Array(nums[i+1..<nums.count])
-            _combine(numsLeft, [nums[i]])
-        }
-
+        dfs(1,[Int]())
         return result
     }
 }
+
+
