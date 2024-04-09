bcf13754b33949eae7ea36a3d49c1a36d875123c
JeffConcerto <111106355+JeffConcerto@users.noreply.github.com>
2024-04-09 01:41:19 -0400

    Update 2073 - Time Needed to Buy Tickets.swift

--- swift/2073 - Time Needed to Buy Tickets.swift
+++ swift/2073 - Time Needed to Buy Tickets.swift
@@ -1,3 +1,21 @@
+// New Solution:
+class Solution {
+    func timeRequiredToBuy(_ tickets: [Int], _ k: Int) -> Int {
+       let maxTime = tickets[k]
+       var totalTime = 0
+       for (i, ticket) in tickets.enumerated() {
+        if i <= k {
+            totalTime += min(maxTime, ticket)
+        } else {
+            totalTime += min(maxTime-1, ticket)
+        }
+       } 
+
+       return totalTime
+    }
+}
+
+// - Old Solution:
 class Solution {
     func timeRequiredToBuy(_ tickets: [Int], _ k: Int) -> Int {
         var tickets = tickets
