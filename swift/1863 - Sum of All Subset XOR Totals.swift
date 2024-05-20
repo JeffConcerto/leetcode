90ab91bd92bc5fbe30a649e26a008407751fbb27
JeffConcerto <111106355+JeffConcerto@users.noreply.github.com>
2024-05-20 12:45:33 -0400

    Update 1863 - Sum of All Subset XOR Totals.swift

--- swift/1863 - Sum of All Subset XOR Totals.swift
+++ swift/1863 - Sum of All Subset XOR Totals.swift
@@ -1,23 +1,17 @@
 class Solution {
     func subsetXORSum(_ nums: [Int]) -> Int {
-        var subsets: [[Int]] = [[]]
-        
-        for num in nums {
-            for set in subsets {
-                subsets.append(set + [num])
-                
-            }
-        }
-        
         var sum = 0
-        
-        for set in subsets where !set.isEmpty {
-            var total = 0
-            for index in 0..<set.count {
-              total ^= set[index]  
-            }
-            sum += total
+
+       func dfs(_ i: Int, _ orSum: Int) {
+        if i == nums.count {
+            sum += orSum
+        } else {
+            dfs(i+1, orSum)
+            dfs(i+1, orSum ^ nums[i])
         }
-        return sum
+       }
+
+       dfs(0,0)
+       return sum
     }
 }
