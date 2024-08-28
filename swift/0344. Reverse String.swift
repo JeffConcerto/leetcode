52bb62270f7a43b3358b55963fba4baba9aacb8c
JeffConcerto <111106355+JeffConcerto@users.noreply.github.com>
2024-08-28 15:02:36 -0400

    Update 0344. Reverse String.swift
    Added new solution

--- swift/0344. Reverse String.swift
+++ swift/0344. Reverse String.swift
@@ -1,3 +1,13 @@
+// New Solution:
+class Solution {
+    func reverseString(_ s: inout [Character]) {
+        for i in 0..<s.count/2 {
+            s.swapAt(i,s.count-1-i)
+        }
+    }
+}
+
+// Old Solution:
 class Solution {
     func reverseString(_ s: inout [Character]) {
         var front = 0
