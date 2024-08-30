5299c042dc55c60962d7c7391d0e598291442aa1
JeffConcerto <111106355+JeffConcerto@users.noreply.github.com>
2024-08-30 16:47:04 -0400

    Update 0077 - Combinations.swift
    Updated Solution

--- swift/0077 - Combinations.swift
+++ swift/0077 - Combinations.swift
@@ -1,21 +1,21 @@
 class Solution {
     func combine(_ n: Int, _ k: Int) -> [[Int]] {
+        // k = total numbers
+        // 1..n = range
+        // all combinations
+        // any order
         var result = [[Int]]()
 
-        func dfs(_ i: Int, _ current: [Int]) {
-            if current.count == k {
-                result.append(current)
-            } else if i > n {
-                return
-            } else {
-                dfs(i+1, current + [i])
-                dfs(i+1, current)
+       func helper(_ start: Int, _ current: [Int]) {
+        if current.count == k {
+            result.append(current)
+        } else if start <= n {
+            for i in start...n {
+                helper(i+1, current + [i])
             }
         }
-
-        dfs(1,[Int]())
+       }
+       helper(1, [])
         return result
     }
 }
-
-
