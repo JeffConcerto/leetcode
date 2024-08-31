7fa6b8cb05dcce686e697af5249940f4edde6d22
JeffConcerto <111106355+JeffConcerto@users.noreply.github.com>
2024-08-31 15:56:59 -0400

    Update 1614 - Maximum Nesting Depth of the Parentheses.swift

--- swift/1614 - Maximum Nesting Depth of the Parentheses.swift
+++ swift/1614 - Maximum Nesting Depth of the Parentheses.swift
@@ -1,3 +1,23 @@
+// New Solution:
+class Solution {
+    func maxDepth(_ s: String) -> Int {
+        var leftParentheses = 0
+        var maxi = 0
+
+        for char in s {
+            if char == "(" {
+                leftParentheses += 1
+            } else if char == ")" {
+                leftParentheses -= 1
+            }
+            maxi = max(leftParentheses, maxi)
+        }
+
+        return maxi
+    }
+}
+
+// Old Solution:
 class Solution {
     func maxDepth(_ s: String) -> Int {
         var stack = [Character]()
