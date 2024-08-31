b6b733a89d320415ec76036ab4e758d416a8f5c7
JeffConcerto <111106355+JeffConcerto@users.noreply.github.com>
2024-08-30 21:27:28 -0400

    Update 2396. Strictly Palindromic Number.swift

--- swift/2396. Strictly Palindromic Number.swift
+++ swift/2396. Strictly Palindromic Number.swift
@@ -1,3 +1,13 @@
+// New Solution:
+// Given the constraints of n >= 4 && n-2 base, the answer will always be false beause the n-2 base will be 1 2 and not a palindrome
+
+class Solution {
+    func isStrictlyPalindromic(_ n: Int) -> Bool {
+        return false
+    }
+}
+
+// Old Solution:
 class Solution {
     func isStrictlyPalindromic(_ n: Int) -> Bool {
         for i in 2...n-2 {
@@ -8,7 +18,7 @@ class Solution {
                 num /= i
             }
 
-            for i in 0..<base.count {
+            for i in 0..<base.count/2 {
                 if base[i] != base[base.count-1-i] { return false}
             }
         }
