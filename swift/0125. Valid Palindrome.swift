80cd0332071c62b1553943be25414db63eb31e9a
JeffConcerto <111106355+JeffConcerto@users.noreply.github.com>
2024-09-01 23:57:16 -0400

    Update 0125. Valid Palindrome.swift
    New Solution

--- swift/0125. Valid Palindrome.swift
+++ swift/0125. Valid Palindrome.swift
@@ -1,3 +1,18 @@
+// New Solution:
+class Solution {
+    func isPalindrome(_ s: String) -> Bool {
+        let s = Array(s.lowercased().filter { $0.isLetter || $0.isNumber })
+        var left = 0
+        var right = s.count-1
+        for i in 0..<s.count {
+            if s[i] != s[s.count-1-i] { return false }
+        }
+
+        return true
+    }
+}
+
+// Old Solution:
 class Solution {
     func isPalindrome(_ s: String) -> Bool {
         let s = Array(s)
