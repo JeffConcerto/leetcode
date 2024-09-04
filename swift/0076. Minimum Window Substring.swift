52dd14fbe3756cfd9ed0cfe49b006e7f25ffbe2b
JeffConcerto <111106355+JeffConcerto@users.noreply.github.com>
2024-09-04 15:40:34 -0400

    Update 0076. Minimum Window Substring.swift
    Most recent solution with no help

--- swift/0076. Minimum Window Substring.swift
+++ swift/0076. Minimum Window Substring.swift
@@ -1,3 +1,44 @@
+// Most recent solution:
+class Solution {
+    func minWindow(_ s: String, _ t: String) -> String {
+        guard s.count >= t.count else { return "" }
+        let tMap = t.reduce(into:[:]) { $0[$1, default:0] += 1 }
+        let s = Array(s)
+        var sMap = [Character:Int]()
+        var left = 0
+        var start = -1
+        var end = 10_0000
+        for right in 0..<s.count {
+            let char = s[right]
+            if tMap[char] != nil {
+                sMap[char, default: 0] += 1 
+            } 
+
+            while left <= right && tMap.keys == sMap.keys && equalMaps(base: tMap, copy: sMap) {
+                if right-left < end-start {
+                    start = left
+                    end = right
+                }
+                if let value = sMap[s[left]] {
+                    sMap[s[left]] = value == 1 ? nil : value-1
+                }
+                left += 1
+            }
+        }
+
+        return start == -1 ? "" : String(s[start...end])
+    }
+
+    private func equalMaps(base: [Character:Int], copy: [Character:Int]) -> Bool {
+        for (character, freq) in base {
+            if copy[character]! < freq { return false }
+        } 
+
+        return true
+    }
+}
+
+// Solution:
 class Solution {
     func minWindow(_ s: String, _ t: String) -> String {
         let tMap = t.reduce(into:[:]) { $0[$1, default:0] += 1 }
