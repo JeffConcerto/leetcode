9086da6976a6e86d23c43363c035292ac66e4111
JeffConcerto <111106355+JeffConcerto@users.noreply.github.com>
2024-09-06 14:59:19 -0400

    Update 0150. Evaluate Reverse Polish Notation.swift
    Added stack solution

--- swift/0150. Evaluate Reverse Polish Notation.swift
+++ swift/0150. Evaluate Reverse Polish Notation.swift
@@ -1,3 +1,39 @@
+// Stack Solution:
+class Solution {
+    func evalRPN(_ tokens: [String]) -> Int {
+        var operands = [Int]()
+        var sum = 0
+        for token in tokens {
+            if let number = Int(token) {
+                operands.append(number)
+            } else {
+                let token2 = operands.removeLast()
+                let token1 = operands.removeLast()
+                operands.append(doOperation(token,token1, token2))
+            }
+        }
+
+        return operands[0]
+    }
+
+    private func doOperation(_ operation: String, _ token1: Int, _ token2: Int) -> Int {
+        switch operation {
+            case "+":
+            return token1 + token2
+            case "-":
+            return token1 - token2
+            case "/":
+            return token1 / token2
+            case "*":
+            return token1 * token2
+            default:
+            return 0
+        }
+    }
+}
+
+
+// Recursive Solution:
 class Solution {
     func evalRPN(_ tokens: [String]) -> Int {
         var tokens = tokens
