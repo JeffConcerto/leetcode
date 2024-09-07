954d51c8ea2571f93320628d1408323f853b4279
JeffConcerto <111106355+JeffConcerto@users.noreply.github.com>
2024-09-07 16:13:50 -0400

    Update 0981. Time Based Key-Value Store.swift
    Added new solution

--- swift/0981. Time Based Key-Value Store.swift
+++ swift/0981. Time Based Key-Value Store.swift
@@ -1,3 +1,42 @@
+// New Solution:
+
+class TimeMap {
+    private var map = [String:[(Int, String)]]()
+    init() {}
+    
+    func set(_ key: String, _ value: String, _ timestamp: Int) {
+        map[key, default: []].append((timestamp,value))
+    }
+    
+    func get(_ key: String, _ timestamp: Int) -> String {
+        guard let timeValues = map[key] else { return "" }
+
+        var left = 0
+        var right = timeValues.count-1
+
+        while left <= right {
+            let mid = left + (right-left)/2
+
+            if timeValues[mid].0 < timestamp {
+                left = mid+1
+            } else if timeValues[mid].0 > timestamp {
+                right = mid-1
+            } else {
+                return timeValues[mid].1
+            }
+        }
+        // wasn't found, return previous
+        return right >= 0 ? timeValues[right].1 : ""
+    }
+}
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * let obj = TimeMap()
+ * obj.set(key, value, timestamp)
+ * let ret_2: String = obj.get(key, timestamp)
+ */
+
 // Keep track of timestamp in array. append each new stamp. will be in sorted order.
 // then binary search to find first <= stamp.
 class TimeMap {
