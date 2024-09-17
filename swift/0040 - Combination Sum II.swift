423999aace0b1395218e1aa484546c429165d1cf
JeffConcerto <111106355+JeffConcerto@users.noreply.github.com>
2024-09-17 17:25:22 -0400

    Update 0040 - Combination Sum II.swift
    New simple Solution

--- swift/0040 - Combination Sum II.swift
+++ swift/0040 - Combination Sum II.swift
@@ -1,63 +1,17 @@
 class Solution {
-    func combinationSum2(_ candidates: [Int], _ target: Int) -> [[Int]] {
-        var result: Set<[Int]> = []
-        var candidates = candidates.sorted()
-        let total = candidates.reduce(0,+)
-        if total < target { return []}
-        if total == target { return [candidates]}
-      
-        helper(candidates, target, [], &result, total)
-        
-        return Array(result)
-    }
-    
-    private func helper(_ candidates: [Int], _ target: Int, _ selection: [Int], _ result: inout Set<[Int]>, _ total: Int) {
-        guard target != 0 else { result.insert(selection); return }
-        guard total >= target else { return }
-        guard total != target else {
-            let selection = selection + candidates
-            result.insert(selection)
-            return
+    func combinationSum(_ can: [Int], _ target: Int) -> [[Int]] {
+        var result = [[Int]]()
+        func dfs(_ i: Int, _ sum: Int, _ selected: [Int]) {
+            if sum == target {
+                result.append(selected)
+            } else {
+                if i == can.count || sum > target { return }
+                dfs(i, sum+can[i], selected + [can[i]])
+                dfs(i+1, sum, selected)
+            }
         }
-        guard !candidates.isEmpty && target > 0 else { return }
-                
-        var nextIndex = 1
-        var total = total
-      
-        helper(Array(candidates[nextIndex..<candidates.count]), target - candidates[0], selection + [candidates[0]], &result, total - candidates[0])
-        
-          while nextIndex < candidates.count && candidates[0] == candidates[nextIndex] {
-            total -= candidates[nextIndex]
-            nextIndex += 1
-        }
-      
-        helper(Array(candidates[nextIndex..<candidates.count]), target, selection, &result, total - candidates[0])
-    }
-}
 
-// Method 2 - Not tracking total:
-class Solution {
-    func combinationSum2(_ candidates: [Int], _ target: Int) -> [[Int]] {
-        var result: Set<[Int]> = []
-        var candidates = candidates.sorted()
-      
-        helper(candidates, target, [], &result)
-        
-        return Array(result)
-    }
-    
-    private func helper(_ candidates: [Int], _ target: Int, _ selection: [Int], _ result: inout Set<[Int]>) {
-        guard target != 0 else { result.insert(selection); return }
-        guard !candidates.isEmpty && target >= 0 else { return }
-                
-        var nextIndex = 1
-      
-        helper(Array(candidates[nextIndex..<candidates.count]), target - candidates[0], selection + [candidates[0]], &result)
-        
-          while nextIndex < candidates.count && candidates[0] == candidates[nextIndex] {
-            nextIndex += 1
-        }
-        
-        helper(Array(candidates[nextIndex..<candidates.count]), target, selection, &result)
+        dfs(0, 0, [])
+        return result
     }
 }
