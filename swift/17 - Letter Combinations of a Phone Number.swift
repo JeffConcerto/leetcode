0abe1fef8695e2b7e1cb7dfd2c79f6f6f8a8fe61
JeffConcerto <111106355+JeffConcerto@users.noreply.github.com>
2024-09-18 16:01:02 -0400

    Update 17 - Letter Combinations of a Phone Number.swift
    New Solution

--- swift/17 - Letter Combinations of a Phone Number.swift
+++ swift/17 - Letter Combinations of a Phone Number.swift
@@ -1,41 +1,33 @@
 class Solution {
+    let map = [
+        2:["a", "b", "c"],
+        3:["d", "e", "f"],
+        4:["g", "h", "i"],
+        5:["j", "k", "l"],
+        6:["m", "n", "o"],
+        7:["p", "q", "r", "s"],
+        8:["t", "u", "v"],
+        9:["w", "x", "y", "z"],
+        ]
     func letterCombinations(_ digits: String) -> [String] {
         guard !digits.isEmpty else { return [] }
-        
-        var numMap: [Character: [Character]] = [
-            "2":["a","b","c"],
-            "3":["d","e","f"], 
-            "4":["g","h","i"], 
-            "5":["j","k","l"], 
-            "6":["m","n","o"], 
-            "7":["p","q","r", "s"], 
-            "8":["t","u", "v"], 
-            "9":["w","x","y", "z"]
-        ]
-        
+        let digits = Array(digits)
+        var result = [String]()
 
-        for digit in digits {
-            let letters = numMap[digit]!
-            
-            if letterCombinations.isEmpty {
-                for letter in letters {
-                    letterCombinations.append("\(letter)")
-                }
-            } 
-            else {
-                let combinationCount = letterCombinations.count
-                
-                for _ in 0..<combinationCount {
-                    let string = letterCombinations.removeFirst()
+        func dfs(_ i: Int, _ string: String) {
+            if i == digits.count {
+                result.append(string)
+            } else {
+                if let letters = map[digits[i].wholeNumberValue!] {
                     for letter in letters {
-                        letterCombinations.append("\(string)\(letter)")
+                        dfs(i+1, string + String(letter))
                     }
                 }
             }
         }
+
+        dfs(0, "")
+        return result
         
-        return letterCombinations
     }
 }
-                                          
-                                       
