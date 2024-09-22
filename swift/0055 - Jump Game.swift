892babd4aef92ef2937f3aedfe9dd7342e18ad33
JeffConcerto <111106355+JeffConcerto@users.noreply.github.com>
2024-09-21 21:01:40 -0400

    Update 0055 - Jump Game.swift
    New Solution

--- swift/0055 - Jump Game.swift
+++ swift/0055 - Jump Game.swift
@@ -1,19 +1,12 @@
 class Solution {
     func canJump(_ nums: [Int]) -> Bool {
-        guard nums.count > 1 else { return true }
-        if nums[0] == 0 { return false }
-        for i in 1..<nums.count-1 {
-            guard nums[i] == 0 else { continue }
-            var j = i-1
-            var back = 1
-            var canGoBack = false
-            while j >= 0 {
-                if nums[j] > back { canGoBack = true; break }
-                back += 1
-                j -= 1
-            }
-            if !canGoBack { return false }
-    }
-    return true
+        var last = 0
+
+        for (i,num) in nums.enumerated() {
+            if last < i { return false}
+            last = max(last, num + i)
+        }
+
+        return true
     }
 }
